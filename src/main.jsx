2235f098cdf752e7750d5a51c6ec387362bc31f0
joel2048 <joel.braun@gmx.net>
2025-07-29 11:17:26 +0800

    fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider } from "@auth0/auth0-react";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain="dev-eqq3olqazv3dbccv.us.auth0.com"
     clientId="WzEFOjZefoWbqUc0irBe05MLHZNRv8TX"
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
     }}
   >
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>    
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </Auth0Provider>
-)
+);
